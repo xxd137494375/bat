dcb00a11814f925a359bd51a6cd6a5628d52e140
xxd <137494375@qq.com>
2019-05-08 13:48:26 +0800

    有序数组合并练习

--- src/two/two_12_paixu3/two_15_Merge.java
+++ src/two/two_12_paixu3/two_15_Merge.java
@@ -11,6 +11,17 @@ public class two_15_Merge {
         有两个从小到大排序以后的数组A和B，其中A的末端有足够的缓冲空容纳B。请编写一个方法，将B合并入A并排序。
         给定两个有序int数组A和B，A中的缓冲空用0填充，同时给定A和B的真实大小int n和int m，请返回合并后的数组。
      */
+
+    /**
+     * very easy
+     *
+     * 注意点：只要B放完了 就可以结束了
+     * @param A
+     * @param B
+     * @param n
+     * @param m
+     * @return
+     */
     public int[] mergeAB(int[] A, int[] B, int n, int m) {
         // write code here
         int i=n-1;
@@ -25,6 +36,23 @@ public class two_15_Merge {
 //        }
         while(j>=0){
             A[index--]=B[j--];
+        }
+        return A;
+    }
+
+
+    /**
+     * 答案：省略3个变量
+     */
+    public int[] mergeAB1(int[] A, int[] B, int n, int m) {
+        while (m != 0) {
+            if (n == 0) {
+                A[m - 1] = B[m - 1];
+                m--;
+            } else {
+                A[m + n - 1] = A[n - 1] > B[m - 1] ? A[--n] : B[--m];
+                //通俗一点   A[m+n-1]=A[n-1]>B[m-1]?A[n-1]:B[m-1];  n--; m--
+            }
         }
         return A;
     }
