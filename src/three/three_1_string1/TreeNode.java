d11f163870f5efb872eab2c3f8c33b12a618f192
xxd <137494375@qq.com>
2019-05-24 03:51:22 +0800

    TreeNode数据结构

---
+++ src/three/three_1_string1/TreeNode.java
@@ -0,0 +1,14 @@
+package three.three_1_string1;
+
+/**
+ * @Author ：xingxiangdong
+ * @Date :2019/5/1916:46
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+}
+}
