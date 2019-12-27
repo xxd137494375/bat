a4b9316f3eb4352d25c9a6088be211a7a067eea9
xxd <137494375@qq.com>
2019-12-27 15:57:48 +0800

    链表指定值清楚

--- src/five/five_2_linkedlist2/five_8_ClearValue.java
+++ src/five/five_2_linkedlist2/five_8_ClearValue.java
@@ -3,6 +3,8 @@ package five.five_2_linkedlist2;
 import five.ListNode;
 
 /**
+ * 链表指定值清楚
+ *
  * 现在有一个单链表。链表中每个节点保存一个整数，再给定一个值val，把所有等于val的节点删掉。
  *
  * 给定一个单链表的头结点head，同时给定一个值val，请返回清除后的链表的头结点，保证链表中有不等于该值的其它值。请保证其他元素的相对顺序。
@@ -53,16 +55,16 @@ public class five_8_ClearValue {
     }
 
     /**
-     * 自己想法的改进版，
+     * 自己想法的改进版，添加一个哨兵
      * @param head
      * @param val
      * @return
      */
     public ListNode clear2(ListNode head, int val) {
         // write code here
-        if(head == null)
+        if (head == null) {
             return null;
-
+        }
         ListNode pre = new ListNode(-1);
         pre.next = head;
         ListNode cur = head;
